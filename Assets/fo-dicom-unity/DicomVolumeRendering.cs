0149ae7523a54c2d1a6c977c4e8b2c740dfb3d44
ovensmr <michael.robert.ovens@gmail.com>
2021-07-14 15:46:34 +0800

    added volume render to texture3d

---
+++ Assets/fo-dicom-unity/DicomVolumeRendering.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Dicom.Unity
+{
+    public static class DicomVolumeRendering
+    {
+        public static Texture3D ToTexture3D (this DicomFile[] dicomFiles)
+        {
+            return ToTexture3D(DicomVolumeRenderData.Extract(dicomFiles));
+        }
+
+        public static Texture3D ToTexture3D (this DicomVolumeRenderData renderData)
+        {
+            float[] normalisedValues = DicomRenderingUtilities.NormaliseValues(renderData.houndsfieldValues);
+            Color[] grayscaleColors = DicomRenderingUtilities.ConvertValuesToGrayscale(normalisedValues);
+
+            Texture3D volumeTexture = new Texture3D(renderData.voxelCount.x, renderData.voxelCount.y, renderData.voxelCount.z, TextureFormat.RFloat, false);
+            volumeTexture.SetPixels(grayscaleColors);
+            volumeTexture.Apply();
+
+            return volumeTexture;
+        }
+    }
+}
