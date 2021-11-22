ae732f15ba399651e457ff0eff93f7a884c402e7
ovensmr <michael.robert.ovens@gmail.com>
2021-11-22 15:15:33 +0800

    imported mpuikit

--- Assets/Content/fo-dicom-unity/Scripts/UI/SeriesManager.cs
+++ Assets/Content/fo-dicom-unity/Scripts/UI/SeriesManager.cs
@@ -8,15 +8,40 @@ namespace Dicom.Unity.UI
     public class SeriesManager : MonoBehaviour
     {
         public StudyManager studyManager;
+        public Transform contents;
         public SeriesSelectable prefabSelectable;
+        public Material prefabMaterial;
+
+        private List<SeriesSelectable> activeSeries = new List<SeriesSelectable>();
 
         private void OnEnable() { studyManager.OnStudyLoaded += OnStudyLoaded; }
         private void OnDisable() { studyManager.OnStudyLoaded -= OnStudyLoaded; }
 
         private void OnStudyLoaded(object sender, DicomStudy study)
         {
+            ClearExistingSeries();
+            AddNewSeries(study);
+        }
+        
+        private void ClearExistingSeries()
+        {
+            for (int i = 0; i < activeSeries.Count; i++)
+                Destroy(activeSeries[i].gameObject);
+
+            activeSeries.Clear();
+        }
+
+        private void AddNewSeries(DicomStudy study)
+        {
             foreach (var series in study.series)
-                Instantiate(prefabSelectable, transform);
+            {
+                // Materials need to be manually instanced to work with Unity's Canvas rendering
+                Material instancedMaterial = new Material(prefabMaterial);
+
+                SeriesSelectable selectable = Instantiate(prefabSelectable, contents);
+                selectable.Inject(instancedMaterial, series.Value);
+                activeSeries.Add(selectable);
+            }
         }
     }
 }
--- Assets/Content/fo-dicom-unity/Scripts/UI/SeriesSelectable.cs
+++ Assets/Content/fo-dicom-unity/Scripts/UI/SeriesSelectable.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
 
 namespace Dicom.Unity.UI
 {
@@ -9,5 +12,27 @@ namespace Dicom.Unity.UI
     public class SeriesSelectable : MonoBehaviour
     {
         public DicomSliceRenderer sliceRenderer;
+
+        [SerializeField] private TextMeshProUGUI idLabel;
+        [SerializeField] private TextMeshProUGUI countLabel;
+
+        public DicomSeries series { get; private set; }
+
+        private void OnEnable()
+        {
+            if (series != null)
+                sliceRenderer.Render(series);
+        }
+
+        public void Inject (Material materialInstance, DicomSeries series)
+        {
+            sliceRenderer.GetComponent<RawImage>().material = materialInstance;
+
+            sliceRenderer.Render(series);
+            this.series = series;
+
+            idLabel.text = "ID: " + series.seriesNumber;
+            countLabel.text = series.dicomFiles.Length + " slices";
+        }
     }
 }
